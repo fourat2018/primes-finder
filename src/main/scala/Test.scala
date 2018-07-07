e7153e72a8081240dfd81178a6b7ef1d22c69ac5
fourat1991 <euphrate1991@gmail.com>
2018-07-07 20:01:13 +0200

    test jenkins 2

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -8,7 +8,9 @@ object Test {
   def main(args: Array[String]) {
     val primeSystem = ActorSystem("Prime_Numbers_System")
 
+    
     val superActor = primeSystem.actorOf(Props[SuperActor],"Prime_Actor_Chief")
+    
     superActor ! (1 to 1500)
   }
 
